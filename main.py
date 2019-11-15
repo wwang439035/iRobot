1c2fb65dfac763b8e8234cd7796f0eba9f97ff94
wwang <wwang@marketo.com>
2019-11-14 16:34:59 -0800

    Use sec time

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from controller.map_controller import *
 from controller.search_controller import *
+import time
 
 MAP_SIZE = [800, 600]
-MAP_CHANGE_DURATION = 1000
+MAP_CHANGE_DURATION = 15
 ACTIONS = ['lift', 'down']
 NUM_BOX = 5
 
@@ -13,24 +14,22 @@ def main():
 
     for _ in range(NUM_BOX):
         for action in ACTIONS:
-            time_step = 0
+            start_time = time.time()
             path_index = 0
-            print(time_step)
 
             path = get_path(map_grid, robot_pos, goal_pos)
             render_path(path)
 
-            while time_step < MAP_CHANGE_DURATION and path_index < len(path):
+            while current_time - start_time < MAP_CHANGE_DURATION and path_index < len(path):
                 render_robot(path[path_index])
                 path_index += 1
-                time_step += 1
+                current_time = time.time()
 
             if path_index == len(path):
                 if action == ACTIONS[0]:
                     render_lift()
                 else:
                     render_down()
-
                 continue
 
             map_grid, box_pos = change_map(path[path_index])
